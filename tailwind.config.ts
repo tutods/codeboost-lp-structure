55e32a768af19f35e74efe83557c77647b38e65a
Daniel Sousa <jdaniel.asousa@gmail.com>
2023-04-16 10:29:52 +0100

    feat: add colors to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,8 +4,19 @@ export default {
 	content: ['./src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {
+			colors: {
+				brand: {
+					black: '#1C1C1F',
+					text: '#606272',
+					gray: '#B2B4C0',
+					yellow: '#F7B40D',
+					orange: '#F14C5D',
+					pink: '#EC0393'
+				}
+			},
 			fontFamily: {
-				sans: ['Atyp BL Text', 'sans-serif']
+				sans: ['Atyp BL Text', 'sans-serif'],
+				'sans-alt': ['Inter', 'sans-serif']
 			}
 		}
 	},
