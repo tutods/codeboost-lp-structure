0fc84e1a63fac3f08517f15938f73eb82036650c
Daniel Sousa <jdaniel.asousa@gmail.com>
2023-04-17 07:35:28 +0100

    feat: improve seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,9 +12,20 @@ export default function App({ Component, pageProps }: AppProps) {
         description={
           'O programa Môre Educação irá trazer novos conhecimento através de aulas teóricas e práticas focado no processo de Design Centrado no Usuário'
         }
+        twitter={{
+          cardType: 'summary_large_image'
+        }}
         openGraph={{
           type: 'website',
-          locale: 'pt-BR'
+          locale: 'pt-BR',
+          images: [
+            {
+              url: '/assets/images/cover.webp',
+              alt: 'Môre | Talent Tech',
+              width: 1920,
+              height: 1080
+            }
+          ]
         }}
       />
 
